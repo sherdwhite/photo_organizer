2348a5e0139e8987f653b8403134e007e35d60e3
Sherd White <sherdwhite@users.noreply.github.com>
2020-01-28 20:39:24 -0600

    Added function to handle error case.  Need to refactor organize function.

--- photo_organizer/organize_photos.py
+++ photo_organizer/organize_photos.py
@@ -18,9 +18,16 @@ def organize():
     logger.info("Destination Directory: {}".format(destination_dir))
 
     for root, subdirs, files in os.walk(origin_dir):
+        print(root)
+        if not os.listdir(root):
+            os.rmdir(root)
+            print('Removing directory {}'.format(root))
         for file in files:
             file_dir = '{0}/{1}'.format(root, file)
-            logger.debug('At file: {}'.format(file_dir))
+            if file in {'Thumbs.db', 'desktop.ini'}:
+                os.remove(file_dir)
+                continue
+            print('At file: {}'.format(file_dir))
             try:
                 with open(file_dir, 'rb') as image_file:
                     my_image = exif.Image(image_file)
@@ -36,16 +43,38 @@ def organize():
                         if not os.path.exists(file_destination):
                             logger.debug('Moving file {0} to {1}'.format(file, file_destination))
                             shutil.copy(file_dir, file_destination)
+                            image_file.close()
+                            os.remove(file_dir)
+                        else:
+                            image_file.close()
+                            os.remove(file_dir)
                     else:
                         logger.error(
                             'File {0} at location {1} has no exif data.'.format(file, file_dir))
+                        handle_error_cases(destination_dir, file, image_file, file_dir)
                     image_file.close()
             except (AssertionError, AttributeError) as ex:
                 logger.error('File {0} at location {1} could not be moved. Error: {2}'.format(file, file_dir, ex))
-                image_file.close()
+                handle_error_cases(destination_dir, file, image_file, file_dir)
                 continue
             except TypeError as ex:
                 logger.error('File {0} at location {1} could not be moved, missing datetime. '
                              'Error: {2}'.format(file, file_dir, ex))
-                image_file.close()
+                handle_error_cases(destination_dir, file, image_file, file_dir)
                 continue
+            except ValueError as ex:
+                logger.error('File {0} at location {1} has possible bad exif data. Error: {2}'
+                             'Error: {2}'.format(file, file_dir, ex))
+                handle_error_cases(destination_dir, file, image_file, file_dir)
+                continue
+
+
+def handle_error_cases(destination_dir, file, image_file, file_dir):
+    file_destination = ('{0}/{1}/{2}'.format(destination_dir, 'Unknown', file))
+    folder_destination = ('{0}/{1}'.format(destination_dir, 'Unknown'))
+    if not os.path.exists(folder_destination):
+        os.makedirs(folder_destination)
+    logger.debug('Moving file {0} to {1}'.format(file, file_destination))
+    shutil.copy(file_dir, file_destination)
+    image_file.close()
+    os.remove(file_dir)
